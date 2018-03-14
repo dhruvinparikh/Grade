4389b48f3f910006ba3a1d2ff64cc088c242bfd5
Dhruvin <n01150847@humbermail.ca>
2018-03-13 22:32:15 -0400

    Initial Commit

---
+++ Grade/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Grade
+//
+//  Created by Kamlesh Shah on 2018-03-13.
+//  Copyright © 2018 Dhruvin Parikh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
