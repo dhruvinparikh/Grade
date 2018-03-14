8f2a6ae298c8758928732f2ab9ae4817bf03309c
Dhruvin <n01150847@humbermail.ca>
2018-03-13 23:32:08 -0400

    Objects created, tested

--- Grade/GradeBook.swift
+++ Grade/GradeBook.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+public class Gradebook{
+    public var student_id: Int
+    public var name: String
+    public var course1:Double
+    public var course2:Double
+    public var course3:Double
+    
+    //One designated initializer which initializes all the properties with values.
+    public init(student_id: Int, name: String, course1:Double,course2:Double,course3:Double){
+        self.student_id=student_id
+        self.name=name
+        //no negative values for marks
+        if(course1 >= 0){
+            self.course1=course1
+        }
+        else{
+            self.course1=0
+        }
+        if(course2 >= 0){
+            self.course2=course2
+        }
+        else{
+            self.course2=0
+        }
+        if(course3 >= 0){
+            self.course3=course3
+        }
+        else{
+            self.course3=0
+        }
+    }
+    
+    public func calculateAverageGrade()->Double{
+            return (course1+course2+course3)/3
+    }
+    
+    public func toString() -> String{
+        return "StudentID : " + String(self.student_id) + ", name : " + self.name + ", course1 : " + String(self.course1) + ", course2 : " + String(self.course2) + ", course3 : " + String(self.course3)
+    }
+    
+}
--- Grade/main.swift
+++ Grade/main.swift
@@ -8,5 +8,11 @@
 
 import Foundation
 
-print("Hello, World!")
+let grade1 = Gradebook(student_id: 17, name: "Dhruvin", course1: 89, course2: 90, course3: 95)
+print(grade1.toString())
+print("Average Grade is ",grade1.calculateAverageGrade())
+
+let grade2 = Gradebook(student_id: 18, name: "David", course1: 90, course2: 84, course3: 86)
+print(grade2.toString())
+print("Average Grade is ",grade2.calculateAverageGrade())
 
